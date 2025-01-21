1fa057478d321b0c716bb52ad0f26a28949aac9d
soner-goit <aribbonofblackk@gmail.com>
2025-01-21 23:37:34 +0300

    Added task-2.js with makeArray function

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,14 @@
-function formatMessage(message, maxLength) {
-  // Eğer mesaj uzunluğu maxLength'e eşit veya daha kısa ise mesajı olduğu gibi döndür
-  if (message.length <= maxLength) {
-      return message;
-  } else {
-      // Mesajı kısalt ve sonuna "..." ekle
-      return message.slice(0, maxLength) + "...";
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    newArray.length = maxLength;
   }
+  return newArray;
 }
 
-// Testler
-console.log(formatMessage("Curabitur ligula sapien", 16));  // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23));  // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20));  // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30));  // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));  // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));  // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
